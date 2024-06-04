3fb7f523fbd3f8665d69baeed0837555955b612a
ClimenteA <climente.alin@gmail.com>
2024-06-04 15:17:00 +0300

    feat: finally hot reload

--- index.ts
+++ index.ts
@@ -1,35 +1,72 @@
 import fs from "node:fs"
 import nunjucks from "nunjucks"
+import { watch } from "fs"
 
-
-nunjucks.configure('site', { autoescape: true })
 
 if (!fs.existsSync("dist")) fs.mkdirSync("dist")
 
 let routes: { [key: string]: string } = {}
 
-fs.readdirSync("./site/pages").forEach(filename => {
+function makeRoutes() {
 
-    let noExtFilename = filename.replace(".nj", "")
-    let htmlFilename = filename.replace(".nj", ".html")
+    nunjucks.configure('site', { autoescape: true })
 
-    fs.writeFileSync(`./dist/${htmlFilename}`, nunjucks.render(`pages/${filename}`))
+    fs.readdirSync("./site/pages").forEach(filename => {
 
-    if (noExtFilename == "index") routes["/"] = `./dist/${htmlFilename}`
-    else routes["/" + noExtFilename] = `./dist/${htmlFilename}`
+        let noExtFilename = filename.replace(".nj", "")
+        let htmlFilename = filename.replace(".nj", ".html")
+        let exportHTMLFilepath = `./dist/${htmlFilename}`
 
-})
+        fs.writeFileSync(exportHTMLFilepath, nunjucks.render(`pages/${filename}`))
 
+        if (noExtFilename == "index") routes["/"] = exportHTMLFilepath
+        else routes["/" + noExtFilename] = exportHTMLFilepath
+
+    })
+}
+
+let filesChanged = true
 
 let server = Bun.serve({
     fetch(req) {
+
+        if (filesChanged == true) {
+            if (process.env.DEBUG == '1' || process.env.DEBUG == undefined) {
+                console.log("Recreating html files...")
+                makeRoutes()
+                console.log("Done recreating html files!")
+            }
+        }
+
         const url = new URL(req.url)
-        if (routes[url.pathname]) return new Response(Bun.file(routes[url.pathname]))
+
+        if (url.pathname.includes("__reload")) {
+            if (filesChanged == true) {
+                filesChanged = false
+                return new Response("Reload true")
+            } else {
+                return new Response("Reload false")
+            }
+        }
+
+        if (routes[url.pathname]) {
+            return new Response(Bun.file(routes[url.pathname]))
+        }
+
         return new Response("Page doesn't exist!")
     },
-    development: process.env.DEBUG == '0',
+    development: process.env.DEBUG == '1',
     port: process.env.PORT || 3000,
 })
 
 
-console.log(`Listening on http://localhost:${server.port} ...`)
+watch(
+    import.meta.dir + "/site",
+    { recursive: true },
+    (event, filename) => {
+        filesChanged = true
+        console.log(`Detected ${event} in ${filename}`)
+    },
+)
+
+console.log(`Listening on http://${server.hostname}:${server.port} ...`)
