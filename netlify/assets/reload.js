5f3b8d1d95fc849b800562428fe1d0ca859135a5
ClimenteA <climente.alin@gmail.com>
2024-06-07 13:21:55 +0300

    feat: separate folder for netlify site

---
+++ netlify/assets/reload.js
@@ -0,0 +1,19 @@
+// TODO: use websockets
+document.addEventListener('DOMContentLoaded', (event) => {
+    let intervalId = setInterval(async () => {
+        try {
+            let response = await fetch('http://localhost:5173/__reload')
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`)
+            }
+            let data = await response.text()
+            if (data === "Reload true") {
+                clearInterval(intervalId)
+                location.reload()
+            }
+        } catch (error) {
+            console.error('Fetch error:', error)
+            clearInterval(intervalId)
+        }
+    }, 800)
+})
